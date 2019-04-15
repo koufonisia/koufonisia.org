52163bd1b14368301f7fbd6c2507d9909c6ff411
George <admin@georgesimos.com>
2019-04-15 22:33:24 +0300

    make it dynamic with node.js

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+// Register the partials
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve 
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        author: 'George Simos',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log('app is running...')
+})
