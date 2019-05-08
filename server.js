792d78a8a8d7ba4992931977d8e67fde2572e562
George <admin@georgesimos.com>
2019-05-08 12:59:17 +0300

    fix

--- server.js
+++ server.js
@@ -6,3 +6,4 @@ const port = process.env.PORT || 3000
 const httpServer = http.createServer(app)
 
 httpServer.listen(port)
+
--- src/app.js
+++ src/app.js
@@ -18,15 +18,7 @@ hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve 
 app.use(express.static(publicDirectoryPath))
-app.use((req, res, next) => {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        return next();
-    } else {
-        // request was via http, so redirect to https
-        return res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+
 app.get('', (req, res) => {
     res.render('index')
 })
