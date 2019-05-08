e0fcdb5fe0a9813c891b02731f61f13db52bf511
George <admin@georgesimos.com>
2019-05-08 12:34:36 +0300

    No need https in plesk

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const fs = require('fs')
 const app = require('./src/app');
-const https = require('https');
+const http = require('http');
 
 const port = process.env.PORT || 3000
 
-// Certificate
-const privateKey = fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/live/koufonisia.org/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/live/koufonisia.org/cert.pem', 'utf8');
+const httpServer = http.createServer(app)
 
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
-
-const httpsServer = https.createServer(credentials, app)
-
-httpsServer.listen(port, () => console.log(`Server is running!`))
+httpServer.listen(port)
