35f8c1723ad859f2846e9a828defe658c4314575
George <admin@georgesimos.com>
2019-05-08 12:42:36 +0300

    Redirect in https

--- src/app.js
+++ src/app.js
@@ -18,7 +18,15 @@ hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve 
 app.use(express.static(publicDirectoryPath))
-
+app.use((req, res, next) => {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 app.get('', (req, res) => {
     res.render('index')
 })
