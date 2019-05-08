257e55584435a8bbe25c700eb7421917e7acfb9e
George <admin@georgesimos.com>
2019-05-08 12:15:23 +0300

    adding ssl keys

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const https = require('https');
 
 const port = process.env.PORT || 3000
 
-https.createServer(app).listen(port);
+// Certificate
+const privateKey = fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/live/koufonisia.org/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/usr/local/psa/var/modules/letsencrypt/etc/live/koufonisia.org/cert.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate
+};
+
+const httpsServer = https.createServer(credentials, app)
+
+httpsServer.listen(port, () => console.log(`Server is running!`))
